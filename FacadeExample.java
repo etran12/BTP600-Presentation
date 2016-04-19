702e5722d48c4fff71c16ea6963406ae7016ac63
Elvis Tran <XoreandoX@hotmail.com>
2016-04-19 00:43:51 -0400

    Presentation Files

---
+++ FacadeExample.java
@@ -0,0 +1,45 @@
+/* Complex parts */
+ 
+class CPU {
+    public void freeze() { ... }
+    public void jump(long position) { ... }
+    public void execute() { ... }
+}
+ 
+class Memory {
+    public void load(long position, byte[] data) { ... }
+}
+ 
+class HardDrive {
+    public byte[] read(long lba, int size) { ... }
+}
+ 
+/* Facade */
+ 
+class Computer {
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+ 
+    public Computer() {
+        this.cpu = new CPU();
+        this.memory = new Memory();
+        this.hardDrive = new HardDrive();
+    }
+ 
+    public void startComputer() {
+        cpu.freeze();
+        memory.load(BOOT_ADDRESS, hardDrive.read(BOOT_SECTOR, SECTOR_SIZE));
+        cpu.jump(BOOT_ADDRESS);
+        cpu.execute();
+    }
+}
+ 
+/* Client */
+ 
+class You {
+    public static void main(String[] args) {
+        Computer facade = new Computer();
+        facade.startComputer();
+    }
+}
